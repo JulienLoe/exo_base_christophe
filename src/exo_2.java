1d1e07678adc0059e8474bdec167dc1197f5c6aa
JulienLoe <julien.loeuilleux62@gmail.com>
2023-07-18 22:02:48 +0200

    third commit

--- src/exo_2.java
+++ src/exo_2.java
@@ -4,9 +4,9 @@ public class exo_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Saisir a ?");
+        System.out.println("Saisir a :");
         int a = scanner.nextInt();
-        System.out.println("Saisir b ?");
+        System.out.println("Saisir b :");
         int b = scanner.nextInt();
 
         System.out.println("Les valeurs sont : a = " + a +" et  b = " + b );
